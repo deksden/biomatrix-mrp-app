8174f49d2112ececa68ac5c504e6e9566ed62f4c
deksden <deksden@deksden.com>
2019-05-23 13:24:07 +0700

    - (WIP) mrp files - start implementing

---
+++ src/data-provider/data-provider.js
@@ -0,0 +1,20 @@
+import fakeDataProvider from 'ra-data-fakerest';
+import manufacturings from './manufacturing'
+import process from './process'
+import products from './products'
+import resources from './resources'
+
+const dataProvider = fakeDataProvider({
+    posts: [
+        { id: 0, title: 'Hello, world!' },
+        { id: 1, title: 'FooBar' },
+    ],
+    comments: [
+        { id: 0, post_id: 0, author: 'John Doe', body: 'Sensational!' },
+        { id: 1, post_id: 0, author: 'Jane Doe', body: 'I agree' },
+    ],
+  manufacturings,
+  process,
+  products,
+  resources
+})
