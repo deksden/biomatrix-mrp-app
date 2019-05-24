c0a8d9d6cec035986ca03ea25e6ce8b05f4c22ee
deksden <deksden@deksden.com>
2019-05-24 21:22:00 +0700

    - (enh) some edit styling experiments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin'
+import React from 'react'
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin'
 import { dataProvider } from './data-provider/data-provider'
 import MyLayout from './components/my-layout'
 
-function App() {
+import { resList as ResourceList, resEdit as ResourceEdit } from './resources/resources'
+
+function App () {
   return (
     <Admin appLayout={MyLayout} dataProvider={dataProvider}>
-       <Resource name="products" list={ListGuesser} />
-       <Resource name="resources" list={ListGuesser} />
-       <Resource name="processes" list={ListGuesser} />
-       <Resource name="manufacturings" list={ListGuesser} />
-       <Resource name="stages" list={ListGuesser} options={ { hideFromSideMenu: true } }/>
+      <Resource name='products' list={ListGuesser} />
+      <Resource
+        name='resources'
+        list={ResourceList}
+        edit={ResourceEdit}
+      />
+      <Resource name='processes' list={ListGuesser} />
+      <Resource name='manufacturings' list={ListGuesser} />
+      <Resource name='stages' list={ListGuesser} options={{ hideFromSideMenu: true }} />
     </Admin>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/resources/resources.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { List, Datagrid, TextField, NumberField, DateField,
+  Edit, SimpleForm, TextInput, NumberInput, DateInput } from 'react-admin'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+export const styles = {
+  col1w1: { display: 'inline-block', width: '10%', paddingRight: '10%' },
+  col2w1: { display: 'inline-block', width: '30%' },
+  w50: { width: '50%' },
+  w70: { width: '70%' }
+}
+
+export const resList = props => (
+  <List {...props}>
+    <Datagrid rowClick='edit'>
+      <TextField source='id' />
+      <TextField source='title' />
+      <TextField source='unit' />
+      <NumberField source='minStock' />
+      <NumberField source='minOrder' />
+      <NumberField source='orderPeriod' />
+      <NumberField source='defPrice' />
+      <DateField source='initialQnt.date' />
+      <NumberField source='initialQnt.qnt' />
+      <NumberField source='initialQnt.price' />
+    </Datagrid>
+  </List>
+)
+
+export const resEdit = withStyles(styles)(({ classes, ...props }) => (
+  <Edit {...props}>
+    <SimpleForm classes={classes.w70}>
+      <TextInput source='id' fullWidth formClassName={classes.col1w1} />
+      <TextInput source='unit' fullWidth formClassName={classes.col1w1} />
+      <TextInput source='title' fullWidth formClassName={classes.w50} />
+      <NumberInput source='minStock' />
+      <NumberInput source='minOrder' />
+      <NumberInput source='orderPeriod' />
+      <NumberInput source='defPrice' />
+      <DateInput source='initialQnt.date' />
+    </SimpleForm>
+  </Edit>
+))
