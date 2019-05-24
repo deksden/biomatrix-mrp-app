82e668afdf0d10e4c969b68d8f5e3b4ef24b9d3d
deksden <deksden@deksden.com>
2019-05-24 18:54:47 +0700

    - (enh) standard code style - (enh) process stages moved to separate resource - (enh) custom menu and layout (WIP)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin'
 import { dataProvider } from './data-provider/data-provider'
-import products from './data-provider/products'
-import process from './data-provider/process'
+import MyLayout from './components/my-layout'
 
 function App() {
   return (
-    <Admin dataProvider={dataProvider}>
+    <Admin appLayout={MyLayout} dataProvider={dataProvider}>
        <Resource name="products" list={ListGuesser} />
        <Resource name="resources" list={ListGuesser} />
-       <Resource name="process" list={ListGuesser} />
+       <Resource name="processes" list={ListGuesser} />
        <Resource name="manufacturings" list={ListGuesser} />
+       <Resource name="stages" list={ListGuesser} options={ { hideFromSideMenu: true } }/>
     </Admin>
   );
 }
--- src/data-provider/data-provider.js
+++ src/data-provider/data-provider.js
@@ -1,20 +1,14 @@
 import fakeDataProvider from 'ra-data-fakerest';
 import manufacturings from './manufacturing'
-import process from './process'
+import processes from './process'
 import products from './products'
 import resources from './resources'
+import stages from './process-stages'
 
  export const dataProvider = fakeDataProvider({
-    posts: [
-        { id: 0, title: 'Hello, world!' },
-        { id: 1, title: 'FooBar' },
-    ],
-    comments: [
-        { id: 0, post_id: 0, author: 'John Doe', body: 'Sensational!' },
-        { id: 1, post_id: 0, author: 'Jane Doe', body: 'I agree' },
-    ],
   manufacturings,
-  process,
+  processes,
   products,
-  resources
+  resources,
+  stages
 })
