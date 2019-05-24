4311d7f67afa0dcc7215af572c73bddaf33fbaa6
deksden <deksden@deksden.com>
2019-05-24 20:44:37 +0700

    - (enh) custom app layout

--- src/components/my-layout.js
+++ src/components/my-layout.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Layout } from 'react-admin';
-import MyMenu from './my-menu';
+import React from 'react'
+import { Layout } from 'react-admin'
+import MyMenu from './my-menu'
+import MyAppBar from './my-appbar'
 
-const MyLayout = (props) => <Layout {...props} menu={MyMenu} />;
+const MyLayout = (props) => <Layout {...props} menu={MyMenu} appBar={MyAppBar} />
 
-export default MyLayout;
+export default MyLayout
--- src/components/my-menu.js
+++ src/components/my-menu.js
@@ -1,32 +1,52 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { MenuItemLink, getResources, Responsive } from 'react-admin'
+import { MenuItemLink, getResources, Responsive, translate } from 'react-admin'
 import { withRouter } from 'react-router-dom'
+import DefaultIcon from '@material-ui/icons/ViewList'
+import inflection from 'inflection'
 
-const MyMenu = ({ resources, onMenuClick, logout }) => (
-  <div>
-    {resources
-      .filter(r => {
-        let inMenu = r.hasList
+const translatedResourceName = (resource, translate) =>
+  translate(`resources.${resource.name}.name`, {
+    smart_count: 2,
+    _:
+            resource.options && resource.options.label
+              ? translate(resource.options.label, {
+                smart_count: 2,
+                _: resource.options.label
+              })
+              : inflection.humanize(inflection.pluralize(resource.name))
+  })
 
-        if (inMenu && r.options && r.options.hideFromSideMenu) {
-          inMenu = false
-        }
-        return inMenu
-      })
-      .map(resource => (
-        <MenuItemLink key={resource.name} to={`/${resource.name}`} primaryText={resource.name} onClick={onMenuClick} />
-      ))}
-    <MenuItemLink to='/custom-route' primaryText='Miscellaneous' onClick={onMenuClick} />
+const MyMenu = ({ translate, resources, onMenuClick, logout }) =>
+  (<div>
+    {
+      resources
+        .filter(r => {
+          let inMenu = r.hasList
+          if (inMenu && r.options && r.options.hideFromSideMenu) {
+            inMenu = false
+          }
+          return inMenu
+        })
+        .map(resource => (
+          <MenuItemLink
+            key={resource.name}
+            to={`/${resource.name}`}
+            primaryText={translatedResourceName(resource, translate)}
+            onClick={onMenuClick}
+            leftIcon={
+              resource.icon ? <resource.icon /> : <DefaultIcon />
+            }
+          />
+        ))}
     <Responsive
       small={logout}
       medium={null} // Pass null to render nothing on larger devices
     />
-  </div>
-)
+  </div>)
 
 const mapStateToProps = state => ({
   resources: getResources(state)
 })
 
-export default withRouter(connect(mapStateToProps)(MyMenu))
+export default translate(withRouter(connect(mapStateToProps)(MyMenu)))
